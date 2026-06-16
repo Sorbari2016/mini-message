d036d46e79c847e594ce38091fb401a496b246f3
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2026-06-16 14:16:45 +0100

    Create in-memory db

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,2 +1,13 @@
-
-
+// Create in-memory database
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
