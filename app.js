4b0996786457c91cec1a6a4025610a8b7a9ac26a
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2026-06-16 14:06:38 +0100

    Set up server

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+const PORT = 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Server is running on port ${PORT}`);
+});
