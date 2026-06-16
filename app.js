b8aec98d432eaf607369133e1a91264382a0b2d8
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2026-06-16 15:58:56 +0100

    Add a bit of styles

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const __dirname = path.dirname(__filename);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static("public"));
+
 app.use("/", indexRouter);
 
 app.use((err, req, res, next) => {
