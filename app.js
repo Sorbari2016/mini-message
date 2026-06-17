dd5133878f6587fa2b13b7d748873b7470f5ba19
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2026-06-17 17:00:52 +0100

    Added form ejs to allow users to enter text and author

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
